d2f3f0b65897bad50e21b7f40e56ddd875213fae
Buddy Burden <barefootcoder@gmail.com>
2013-10-25 00:15:48 -0700

    packaging for CPAN: 0.09_01

--- lib/Data/Random.pm
+++ lib/Data/Random.pm
@@ -47,7 +47,7 @@ use vars qw(
 @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 @EXPORT    = qw();
 
-$Data::Random::VERSION = '0.10';
+$Data::Random::VERSION = '0.09_01';
 
 ################################################################################
 # - Subroutines
