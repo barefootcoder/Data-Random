54ce58fbf08cf4078a923cb6676356ca42d52b6c
Buddy Burden <barefootcoder@gmail.com>
2025-10-11 03:30:45 -0700

    increment $VERSION after 0.14 release

--- lib/Data/Random.pm
+++ lib/Data/Random.pm
@@ -49,7 +49,7 @@ use vars qw(
 @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 @EXPORT    = qw();
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 ################################################################################
 # - Subroutines
